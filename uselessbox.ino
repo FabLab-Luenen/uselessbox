2dd7f2231ad35611ff2f5bd5de07d9e508989791
MrHat2010 <mrhat2010@googlemail.com>
2017-04-27 11:56:30 +0200

    Dumme dumme Fehler durch zu wenig Schlaf

--- uselessbox.ino
+++ uselessbox.ino
@@ -1,14 +1,18 @@
 
 #include <Servo.h> //Die Bibliothek Servo wird ins Programm eingeführt
 
-const int  buttonPin = 2; //Die Variable buttonPin wird deklariert und der Wert 2 zugewiesen
+const int  buttonPin = 3; //Die Variable buttonPin wird deklariert und der Wert 2 zugewiesen
+
 int buttonState = 0; //Die Variable buttonState wird deklariert und der Wert 0 zugewiesen
 
 Servo myservo; //Deklaration des Objektes myservo aus der Klasse Servo
 
 void setup() //Hier kommen alle Datendeklarationen und Methodenaufrufe für die Initialisierung hinein
 {
-  pinMode(8, OUTPUT); // Pin D8 als 5V
+  pinMode(4, OUTPUT); //Pin 4 wird als Ausgang definiert
+  pinMode(2, OUTPUT); //Pin 2 wird als Ausgang definiert
+  digitalWrite(4, HIGH); //Pin 4 wird auf High (ca 4,5V) gesetzt
+  digitalWrite(2, LOW); //Pin 2 wird auf Low (GND) gesetzt
   pinMode(buttonPin, INPUT); // Pin D2 als Eingang gesetzt (an diesen Pin wird der Schalter angeschlossen)
   myservo.attach(9); //Servo Steuerleitung liegt an Pin D9
 }
@@ -17,7 +21,7 @@ void loop() //Hier kommt alles rein was in der Endlosschleife gebraucht wird hin
 {
   buttonState = digitalRead(buttonPin); //Abfrage des logischen Spannungspegels an Pin D2
   if (buttonState == HIGH) { //Vergleich mit der Variable buttonState
-       myservo.write(70); //Servo bewegt sich soweit bis der Schalter (buttonPin) umgelegt wird
+       myservo.write(70); //Servo bewegt sich soweit bis der Schalter umgelegt wird
        delay(15); //Warte 15 Millisekunden
        myservo.write(0); //Servo kehrt zur Ausgangsposition zurück
   }
